c094bcfadfe7cbcc591feabd114ef33694e35949
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-05-11 00:34:07 +0300

    Route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi necessitatibus molestiae est. Id expedita a provident, quis numquam, possimus ipsam accusamus minima accusantium ratione repellendus voluptas nam modi deleniti exercitationem?</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>

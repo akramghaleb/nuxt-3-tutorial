02edc9247842d6f878d6864f57cf62009bafd73a
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-05-11 00:28:23 +0300

    Adding pages

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor voluptates rem illo labore itaque sit doloribus. Nostrum enim illo nobis consectetur repellat non atque officia. Eum eligendi veritatis velit corporis.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor voluptates rem illo labore itaque sit doloribus. Nostrum enim illo nobis consectetur repellat non atque officia. Eum eligendi veritatis velit corporis.</p>
+    </div>
+  </template>
+  
+  <script setup>
+
+</script>
+
+<style  scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p{
+        margin: 20px 0;
+    }
+</style>

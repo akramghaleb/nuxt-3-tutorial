4d8211b2a98ba62b8698f1da2305b72cb9ce407d
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-05-11 00:43:15 +0300

    Nuxt Link

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <nuxt-link to="/">Akram Nuxt</nuxt-link>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
         <h2>Products</h2>
     </div>
 </template>

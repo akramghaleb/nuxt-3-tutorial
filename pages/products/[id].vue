d527163618a4bd9888770d2e61b6fd6e22dfdf37
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-05-11 13:12:52 +0300

    Metadata

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,9 @@
 <template>
     <div>
+        <Head>
+            <Title>Akram Nuxt | {{ product.title }}</Title>
+            <Meta name="description" :content="product.description" />
+        </Head>
         <product-details :product="product"></product-details>
     </div>
 </template>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -16,6 +16,13 @@ definePageMeta({
 //fetch the products
 const {data:products} =  await useFetch('https://fakestoreapi.com/products')
 
+useHead({
+    title: 'Akram Nuxt | Merch',
+    meta:[
+        { name: 'description' , content: 'Nuxt 3 Merch'}
+    ]
+})
+
 </script>
 
 <style scoped>

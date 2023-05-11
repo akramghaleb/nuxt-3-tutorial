8a3b127cc2df5eeb642e7ecd8592c5279834c085
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-05-11 15:20:06 +0300

    Dynamic server route

--- pages/about.vue
+++ pages/about.vue
@@ -8,10 +8,12 @@
 </template>
 
 <script setup>
-    const { data } = await useFetch('/api/ninja?name=mario' , {
-        method:'post',
-        body: { age : 30 }
-    });
+    // const { data } = await useFetch('/api/ninja?name=mario' , {
+    //     method:'post',
+    //     body: { age : 30 }
+    // });
+
+    const { data } = await useFetch('/api/currency/GBP')
 </script>
 
 <style scoped>
--- server/api/ninja.js
+++ server/api/ninja.js
@@ -8,6 +8,9 @@ export default defineEventHandler(async (event)=>{
     // handle post data
     const { age } = await readBody(event)
 
+    // api call with privacy key
+    const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=at7KOLA72kRGOLQJtHgUgIeTHCgNbW9g7YjwcjVU')
+
     return {
         message: `Hello, ${name}! You are ${age} years old`
     }

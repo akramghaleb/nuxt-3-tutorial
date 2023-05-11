96edb848345670669639e0de0bbe5d16dc161c61
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2023-05-11 12:58:08 +0300

    Error page

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,7 @@
                     <li><NuxtLink to="/">Home</NuxtLink></li>
                     <li><NuxtLink to="/about">About</NuxtLink></li>
                     <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
+                    <li><NuxtLink to="/products/abc">abc</NuxtLink></li>
                 </ul>
             </nav>
         </header>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -11,6 +11,10 @@
     //fetch the product
     const {data:product} = await useFetch(uri , { key : id});
 
+    if(!product.value){
+        throw createError({ statusCode: 404 , statusMessage : 'Product not found' ,fatal:true})
+    }
+
     definePageMeta({
         layout:'products'
     })
